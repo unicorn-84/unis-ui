b663696835b33e2046d6104d605f164d8c3f1d6a
unicorn-84 <unicorn-84@ya.ru>
2021-03-05 21:35:20 +0300

    Added custom styles

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,14 @@
+import { GlobalStyle } from '../src/shared/global';
+
+export const decorators = [
+  Story => (
+    <>
+      <GlobalStyle />
+      <Story />
+    </>
+  ),
+];
+
 export const parameters = {
   actions: { argTypesRegex: '^on.*' },
   controls: { expanded: true },
