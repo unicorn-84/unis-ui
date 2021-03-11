f0f80bacd9360796b478280a5a6897de833bdf26
unicorn-84 <unicorn-84@ya.ru>
2021-03-11 19:06:39 +0300

    Upgrade the Paragraph component

--- src/components/Typography/Paragraph/Paragraph.component.tsx
+++ src/components/Typography/Paragraph/Paragraph.component.tsx
@@ -1,19 +1,7 @@
-import React, { FC, HTMLAttributes, ReactNode } from 'react';
+import React, { FC, HTMLAttributes } from 'react';
 import StyledParagraph from './Paragraph.styles';
 
 export interface ParagraphProps extends HTMLAttributes<HTMLParagraphElement> {
-  /**
-   * The content of the component.
-   */
-  children?: ReactNode;
-  /**
-   * Class to be applied to the root element.
-   */
-  className?: string;
-  /**
-   * Custom label to be used by screen readers. When provided, an aria-label will be added to the element.
-   */
-  a11yLabel?: string;
   /**
    * Applies the theme typography styles.
    */
@@ -22,15 +10,10 @@ export interface ParagraphProps extends HTMLAttributes<HTMLParagraphElement> {
 
 const Paragraph: FC<ParagraphProps> = ({
   children,
-  className,
-  a11yLabel,
   variant = 'body',
+  ...props
 }) => (
-  <StyledParagraph
-    className={className}
-    aria-label={a11yLabel}
-    variant={variant}
-  >
+  <StyledParagraph variant={variant} {...props}>
     {children}
   </StyledParagraph>
 );
