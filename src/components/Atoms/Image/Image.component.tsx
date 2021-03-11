07239facc0d28b464127dd89b97bffa439f8c8e3
unicorn-84 <unicorn-84@ya.ru>
2021-03-11 18:08:36 +0300

    Fix Image component

--- src/components/Atoms/Image/Image.component.tsx
+++ src/components/Atoms/Image/Image.component.tsx
@@ -19,8 +19,6 @@ export default ({ src, placeholder, ...props }: ImageProps) => {
     const img = new Image();
     img.src = src as string;
     img.addEventListener('load', handleLoad);
-    // FIXME:
-    img.addEventListener('error', () => console.log('error'));
 
     return () => img.removeEventListener('load', handleLoad);
   }, [src]);
