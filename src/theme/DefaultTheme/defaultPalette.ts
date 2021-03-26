5271fdcb5ee09cb072b0e94e29ebe3f0840d70d7
unicorn-84 <unicorn-84@ya.ru>
2021-03-26 20:23:38 +0300

    Changed Palette

--- src/theme/DefaultTheme/defaultPalette.ts
+++ src/theme/DefaultTheme/defaultPalette.ts
@@ -3,16 +3,15 @@ import { IPalette } from '../types';
  * Default palette.
  */
 export const defaultPalette: Partial<IPalette> = {
-  primaryBrand: '#3d0240',
-  secondaryBrand: '#137083',
-  primaryText: '#0a0a0a',
-  secondaryText: '#111',
-  lightText: '#fff',
-  grayText: '#b7b7b7',
-  darkColor: '#0a0a0a',
-  lightColor: '#fff',
-  grayColor: 'b7b7b7',
-  darkFon: '#0a0a0a',
-  lightFon: '#fff',
-  grayFon: '#b7b7b7',
+  brand: { primary: '#3d0240', secondary: '#137083' },
+  text: {
+    primary: '#0a0a0a',
+    secondary: '#111',
+    light: '#fff',
+    gray: '#b7b7b7',
+  },
+  dark: '#0a0a0a',
+  light: '#fff',
+  gray: 'b7b7b7',
+  fon: { dark: '#0a0a0a', light: '#fff', gray: '#b7b7b7' },
 };
