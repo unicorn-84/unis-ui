df174295865e876fa24a8127e1d8de9bcd63832a
unicorn-84 <unicorn-84@ya.ru>
2021-06-12 19:16:43 +0300

    Changed tests for the PaginationItem component.

--- src/components/Atoms/PaginationItem/PaginationItem.test.tsx
+++ src/components/Atoms/PaginationItem/PaginationItem.test.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import PaginationItem from '.';
-//TODO: <PaginationItem />
-describe('PaginationItem', () => {
+
+describe('<PaginationItem />', () => {
   it('should render correctly', () => {
     render(
       <PaginationItem
