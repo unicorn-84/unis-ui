bbe2a99e4d157a0dd28ea63ba7496db81936dc49
unicorn-84 <unicorn-84@ya.ru>
2021-06-12 18:49:56 +0300

    Changed the Box component style

--- src/components/Atoms/Box/Box.styles.tsx
+++ src/components/Atoms/Box/Box.styles.tsx
@@ -2,31 +2,7 @@ import styled, { css } from 'styled-components';
 import { BoxProps } from './Box.component';
 import { DefaultTheme as theme } from '../../../theme';
 
-const getDirectionStyles = ({ direction }: BoxProps) => css`
-  flex-direction: ${direction};
-`;
-
-const getAlignStyles = ({ align }: BoxProps) => css`
-  align-items: ${align};
-`;
-
-const getGrowStyles = ({ grow }: BoxProps) => css`
-  flex-grow: ${grow};
-`;
-
-const getWrapStyles = ({ wrap }: BoxProps) => css`
-  flex-wrap: ${wrap};
-`;
-
-const getShrinkStyles = ({ shrink }: BoxProps) => css`
-  flex-shrink: ${shrink};
-`;
-
-const getJustifyStyles = ({ justify }: BoxProps) => css`
-  justify-content: ${justify};
-`;
-
-const getMarginStyles = ({ margin }: BoxProps) => {
+const getMarginStyle = ({ margin }: BoxProps) => {
   if (margin) {
     if (typeof margin === 'string') {
       return css`
@@ -67,7 +43,7 @@ const getMarginStyles = ({ margin }: BoxProps) => {
   return;
 };
 
-const getPaddingStyles = ({ padding }: BoxProps) => {
+const getPaddingStyle = ({ padding }: BoxProps) => {
   if (padding) {
     if (typeof padding === 'string') {
       return css`
@@ -108,7 +84,7 @@ const getPaddingStyles = ({ padding }: BoxProps) => {
   return;
 };
 
-const getGapStyles = ({ gap, direction, wrap }: BoxProps) => {
+const getGapStyle = ({ gap, direction, wrap }: BoxProps) => {
   if (!wrap && gap) {
     if (direction === 'column' || direction === 'column-reverse') {
       return css`
@@ -127,17 +103,25 @@ const getGapStyles = ({ gap, direction, wrap }: BoxProps) => {
 };
 
 const BoxContainer = styled.div<BoxProps>`
-  list-style: none;
   display: ${({ inline }) => (inline ? 'inline-flex' : 'flex')};
-  ${props => props.direction && getDirectionStyles}
-  ${props => props.align && getAlignStyles}
-  ${props => props.justify && getJustifyStyles}
-  ${props => props.margin && getMarginStyles}
-  ${props => props.padding && getPaddingStyles}
-  ${props => props.grow && getGrowStyles}
-  ${props => props.shrink && getShrinkStyles}
-  ${props => props.wrap && getWrapStyles}
-  ${props => props.gap && getGapStyles};
+
+  flex-direction: ${({ direction }) => direction};
+
+  align-items: ${({ align }) => align};
+
+  justify-content: ${({ justify }) => justify};
+
+  flex-wrap: ${({ wrap }) => wrap};
+
+  flex-shrink: ${({ shrink }) => shrink};
+
+  flex-grow: ${({ grow }) => grow};
+
+  ${({ gap }) => gap && getGapStyle};
+
+  ${({ margin }) => margin && getMarginStyle};
+
+  ${({ padding }) => padding && getPaddingStyle};
 `;
 
 BoxContainer.defaultProps = {
