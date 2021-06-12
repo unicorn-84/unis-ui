016048e1221c18569e48b42956b39ab26446d7e6
unicorn-84 <unicorn-84@ya.ru>
2021-06-12 21:23:17 +0300

    Change styles for the Box component.

--- src/components/Atoms/Box/Box.styles.tsx
+++ src/components/Atoms/Box/Box.styles.tsx
@@ -85,8 +85,7 @@ const getPaddingStyle = ({ padding }: BoxProps) => {
 };
 
 const getGapStyle = ({ gap, direction, wrap }: BoxProps) => {
-  //TODO: (!wrap || wrap === 'nowrap') && gap
-  if (wrap === 'nowrap' && gap) {
+  if ((!wrap || wrap === 'nowrap') && gap) {
     if (direction === 'column' || direction === 'column-reverse') {
       return css`
         & > *:not(:last-child) {
