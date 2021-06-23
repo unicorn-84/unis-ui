5cf8f8c99a6ae859295567ec0f0bbbcb83f9619a
unicorn-84 <unicorn-84@ya.ru>
2021-06-23 08:13:46 +0300

    Added the ColorNames type.

--- src/theme/DefaultTheme/defaultPalette.ts
+++ src/theme/DefaultTheme/defaultPalette.ts
@@ -1,4 +1,4 @@
-import { IPalette } from '../types';
+import { ColorNames, IPalette } from '../types';
 /**
  * Default palette.
  */
@@ -14,4 +14,20 @@ export const defaultPalette: IPalette = {
   light: '#fff',
   gray: 'b7b7b7',
   fon: { dark: '#0a0a0a', light: '#fff', gray: '#b7b7b7' },
+};
+
+export const Colors: Record<ColorNames, string> = {
+  brandPrimary: defaultPalette.brand.primary,
+  brandSecondary: defaultPalette.brand.secondary,
+  textPrimary: defaultPalette.text.primary,
+  textSecondary: defaultPalette.text.secondary,
+  textDark: defaultPalette.text.primary,
+  textLight: defaultPalette.text.light,
+  textGray: defaultPalette.text.gray,
+  dark: defaultPalette.dark,
+  light: defaultPalette.light,
+  gray: defaultPalette.gray,
+  fonDark: defaultPalette.fon.dark,
+  fonLight: defaultPalette.fon.light,
+  fonGray: defaultPalette.fon.gray,
 };
