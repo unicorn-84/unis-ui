88bf1b25ba9d8064a7094729cbb75fbbae266edc
unicorn-84 <unicorn-84@ya.ru>
2022-03-24 18:02:58 +0300

    build(rollup): change config

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
-import * as packageJson from './package.json';
+import packageJson from './package.json';
 
 export default [
   {
