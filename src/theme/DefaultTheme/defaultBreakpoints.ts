d1fbff9b4c90a8fea8f08927df4e692e21a26e27
unicorn-84 <unicorn-84@ya.ru>
2022-03-24 20:59:11 +0300

    refactor: remove theme

--- src/theme/DefaultTheme/defaultBreakpoints.ts
+++
@@ -1,9 +0,0 @@
-import { IBreakpoints } from '../types';
-
-export const defaultBreakpoints: IBreakpoints = {
-  xs: '575px',
-  sm: '767px',
-  md: '991px',
-  lg: '1199px',
-  xl: '1399px',
-};
--- src/theme/types/IBreakpoints.ts
+++
@@ -1,8 +0,0 @@
-export type BreakpointName = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
-
-/**
- * Breakpoints.
- */
-export type IBreakpoints = {
-  [key in BreakpointName]: string;
-};
