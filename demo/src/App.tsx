c8cd659aa856889d5dfe86986f3f2abae7413741
unicorn-84 <unicorn-84@ya.ru>
2022-03-31 00:58:10 +0300

    refactor(basebutton): add forwarding classes to native button

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -4,12 +4,13 @@ import BaseButton from '../../src/components/BaseButton/BaseButton';
 const App = () => {
   const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     const currentTarget = e.currentTarget as HTMLButtonElement;
-    currentTarget.classList.toggle('bg-green-500');
     currentTarget.classList.toggle('text-white');
   };
   return (
     <div className="container max-w-3xl m-auto px-4 mt-10">
-      <BaseButton onClick={handleClick}>Share on Telegram</BaseButton>
+      <BaseButton className="bg-rose-500 rounded-none" onClick={handleClick}>
+        Share on Telegram
+      </BaseButton>
     </div>
   );
 };
