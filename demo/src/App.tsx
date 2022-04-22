76aa45183511c2fe22cd7153bc2542eaf3016971
unicorn-84 <unicorn-84@ya.ru>
2022-04-22 22:01:19 +0300

    chore(demo): change App.tsx

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -4,14 +4,16 @@ import { Button } from '../../src';
 const App = () => {
   return (
     <div className="container max-w-3xl m-auto px-4 mt-10 transition">
-      <Button disabled>Share on Telegram</Button>
-      <Button>Share on Telegram</Button>
-      <Button color="primary">Share on Telegram</Button>
-      <Button color="secondary">Share on Telegram</Button>
-      <Button color="success">Share on Telegram</Button>
-      <Button color="info">Share on Telegram</Button>
-      <Button color="warning">Share on Telegram</Button>
-      <Button color="error">Delete</Button>
+      <div className="space-x-2 space-y-2">
+        <Button disabled>Share on Telegram</Button>
+        <Button>Share on Telegram</Button>
+        <Button color="primary">Share on Telegram</Button>
+        <Button color="secondary">Share on Telegram</Button>
+        <Button color="success">Share on Telegram</Button>
+        <Button color="info">Share on Telegram</Button>
+        <Button color="warning">Share on Telegram</Button>
+        <Button color="error">Delete</Button>
+      </div>
     </div>
   );
 };
