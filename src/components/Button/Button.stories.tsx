b97a15780c316ef55c7d538b6102ed7dcc2d541a
unicorn-84 <unicorn-84@ya.ru>
2022-04-22 20:34:30 +0300

    chore(button): add stories

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import ButtonComponent from './Button';
+
+export default {
+  title: 'Button',
+  component: ButtonComponent,
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof ButtonComponent> = (args) => (
+  <ButtonComponent {...args}>Share</ButtonComponent>
+);
+
+export const Button = Template.bind({});
