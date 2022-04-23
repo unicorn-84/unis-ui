7fe4c0cb44533673b711dcc0100b20a917b4a698
Vladimir Kapitonov <unicorn-84@ya.ru>
2022-04-23 18:39:15 +0300

    feat(components): add `Link` (#128)
    * feat(components): add `Link`
    
    * style(link): change `ILinkProps`
    
    * docs(link): add `href`

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { Button } from '../../src';
+import { Link } from '../../src';
 
 const App = () => {
   return (
     <div className="container max-w-xl m-auto px-4 mt-10 transition">
       <div className="space-x-4 flex">
-        <Button variant="outlined" className="flex-1">
+        <Link href="" variant="contained" className="flex-1">
           Submit
-        </Button>
+        </Link>
 
-        <Button variant="outlined" color="secondary" className="flex-1">
+        <Link href="" variant="contained" color="secondary" className="flex-1">
           Reset
-        </Button>
+        </Link>
       </div>
     </div>
   );
