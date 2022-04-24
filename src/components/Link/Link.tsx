8e8965afa7c49fb6e5319c12b6d1e9df346cbc15
unicorn-84 <unicorn-84@ya.ru>
2022-04-24 23:41:03 +0300

    refactor(components): change classes
    remove shadow classes
    
    BREAKING CHANGE: remove some tailwind classes

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -4,21 +4,21 @@ const classes = {
   primary: {
     text: 'tracking-wide text-emerald-500',
     contained:
-      'inline-block rounded px-4 py-2 font-medium tracking-wide transition-colors bg-emerald-500 hover:bg-emerald-600 text-white border-2 border-emerald-500 hover:border-emerald-600 shadow-md shadow-emerald-500/30 text-center',
+      'inline-block rounded px-4 py-2 font-medium tracking-wide transition-colors bg-emerald-500 hover:bg-emerald-600 text-white border-2 border-emerald-500 hover:border-emerald-600 text-center',
     outlined:
       'inline-block rounded px-4 py-2 font-medium tracking-wide border-2 border-emerald-500 ring-emerald-500 text-emerald-500 hover:ring-1 transition-shadow text-center',
   },
   secondary: {
     text: 'tracking-wide text-sky-500',
     contained:
-      'inline-block rounded px-4 py-2 font-medium tracking-wide transition-colors bg-sky-500 hover:bg-sky-600 text-white border-2 border-sky-500 hover:border-sky-600 shadow-md shadow-sky-500/30 text-center',
+      'inline-block rounded px-4 py-2 font-medium tracking-wide transition-colors bg-sky-500 hover:bg-sky-600 text-white border-2 border-sky-500 hover:border-sky-600 text-center',
     outlined:
       'inline-block rounded px-4 py-2 font-medium tracking-wide border-2 border-sky-500 ring-sky-500 text-sky-500 hover:ring-1 transition-shadow text-center',
   },
   link: {
     text: 'tracking-wide text-blue-500',
     contained:
-      'inline-block rounded px-4 py-2 font-medium tracking-wide transition-colors bg-blue-500 hover:bg-blue-600 text-white border-2 border-blue-500 hover:border-blue-600 shadow-md shadow-blue-500/30 text-center',
+      'inline-block rounded px-4 py-2 font-medium tracking-wide transition-colors bg-blue-500 hover:bg-blue-600 text-white border-2 border-blue-500 hover:border-blue-600 text-center',
     outlined:
       'inline-block rounded px-4 py-2 font-medium tracking-wide border-2 border-blue-500 ring-blue-500 text-blue-500 hover:ring-1 transition-shadow text-center',
   },
